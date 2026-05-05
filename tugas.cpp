548f16dfa7441a1bdaa109eac4ee2fb30b8aca61
abil0 <hoenabil6@gmail.com>
2026-05-05 10:36:48 +0700

    input func

--- tugas.cpp
+++ tugas.cpp
@@ -15,6 +15,11 @@ public:
         cout << "Masukkan panjang diagonal 2 : "; cin >> d2;
         cout << "Masukkan panjang sisi       : "; cin >> sisi;
     }
+      
+    double luas() {
+        return 0.5 * d1 * d2;
+    }
+
 
  
 
