5c3b4984fba78bff31ac7b176ed5cfc92ab058c6
abil0 <hoenabil6@gmail.com>
2026-05-05 10:36:21 +0700

    input void belahketupat

--- tugas.cpp
+++ tugas.cpp
@@ -9,6 +9,14 @@ class BelahKetupat {
     double d1, d2, sisi;
 
 public:
+    void input() {
+        cout << "\n--- Input Data Belah Ketupat ---" << endl;
+        cout << "Masukkan panjang diagonal 1 : "; cin >> d1;
+        cout << "Masukkan panjang diagonal 2 : "; cin >> d2;
+        cout << "Masukkan panjang sisi       : "; cin >> sisi;
+    }
+
+ 
 
 
 
